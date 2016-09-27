26c31ba85b148233fdca1b3e4bc50297be05956f
Yukio MURAKAMI <m_yukio@mac.com>
2016-09-27 22:14:34 +0900

    create project

---
+++ Wibree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wibree
+//
+//  Created by 村上幸雄 on 2016/09/27.
+//  Copyright © 2016年 Yukio MURAKAMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
